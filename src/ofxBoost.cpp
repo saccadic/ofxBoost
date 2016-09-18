9cc31a1d17c5c259a3baefc80e6314b1679c9a52
Katsuyoshi Hotta <scanf@me.com>
2016-09-18 21:37:24 +0900

    初回コミット

---
+++ src/ofxBoost.cpp
@@ -0,0 +1,9 @@
+//
+//  ofxBoost.cpp
+//  myIO
+//
+//  Created by Hotta Katusyoshi on 2016/03/06.
+//
+//
+
+#include "ofxBoost.hpp"
---
+++ src/ofxBoost.hpp
@@ -0,0 +1,18 @@
+//
+//  ofxBoost.hpp
+//  myIO
+//
+//  Created by Hotta Katusyoshi on 2016/03/06.
+//
+//
+
+#ifndef ofxBoost_hpp
+#define ofxBoost_hpp
+
+#include "ofMain.h"
+
+class ofxBoost{
+    
+};
+
+#endif /* ofxBoost_hpp */
